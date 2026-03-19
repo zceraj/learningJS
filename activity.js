56bac91567e2c96388be9bcb069af72f4d0de9b5
Zara Ceraj <167219401+zceraj@users.noreply.github.com>
2026-03-19 17:42:11 -0400

    comments
    Fix typo in attribute parameter and add comments.

--- activity.js
+++ activity.js
@@ -1,9 +1,13 @@
 function getElementsByAttribute(attribute, value) {
+  // regex of all elements --> find all the elements 
   var all = document.getElementsByTagName('*');
+  // empty array 
   var found = [];
-  
+
+  // loop through all the attributes 
   for (var i = 0; i < all. length; i++){ 
     element = all[i]; 
+    //add it to the array if it has all the attribute
     if (all[i].getAttribute(attriute)==value){ 
       found.push(all[i]);
     }
