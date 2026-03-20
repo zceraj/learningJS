9f0b1c763d4ab09f6d84336770a4764d6811baab
zceraj <167219401+zceraj@users.noreply.github.com>
2026-03-20 01:15:44 -0400

    Update jsonhandling script

--- Assignment3/jsonhandling.js
+++ Assignment3/jsonhandling.js
@@ -6,3 +6,31 @@ fetch('https://api.github.com/users/zceraj')
     console.log(j);
     document.getElementById('username').innerText = j.login;
   })
+  .then(function(e) {
+    console.log(e);
+    document.getElementById('avatar').innerText = e.avatar_url;
+  })
+  .then(function(e) {
+    console.log(e);
+    document.getElementById('realname').innerText = e.name;
+  })
+  .then(function(e) {
+    console.log(e);
+    document.getElementById('location').innerText = e.location;
+  })
+  .then(function(e) {
+    console.log(e);
+    document.getElementById('bio').innerText = e.bio;
+  })
+  .then(function(e) {
+    console.log(e);
+    document.getElementById('num-followers').innerText = e.followers;
+  })
+  .then(function(e) {
+    console.log(e);
+    document.getElementById('loading').innerText = 'Loading...';
+  }); 
+  // display avatar, username, real name, location, bio, and num followers 
+  // get username and avatar of followers 
+  // add a loading indicator 
+  // add an input box that takes in other names to display 
