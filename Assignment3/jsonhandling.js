b005043c7940daad29c56747d7011107dfb03167
zceraj <167219401+zceraj@users.noreply.github.com>
2026-03-20 08:57:37 -0400

    git

--- Assignment3/jsonhandling.js
+++ Assignment3/jsonhandling.js
@@ -1,36 +1,55 @@
-fetch('https://api.github.com/users/zceraj')
-  .then(function(r) {
-    return r.json();
-  })
-  .then(function(j) {
-    console.log(j);
-    document.getElementById('username').innerText = j.login;
-  })
-  .then(function(e) {
-    console.log(e);
-    document.getElementById('avatar').innerText = e.avatar_url;
-  })
-  .then(function(e) {
-    console.log(e);
-    document.getElementById('realname').innerText = e.name;
-  })
-  .then(function(e) {
-    console.log(e);
-    document.getElementById('location').innerText = e.location;
-  })
-  .then(function(e) {
-    console.log(e);
-    document.getElementById('bio').innerText = e.bio;
-  })
-  .then(function(e) {
-    console.log(e);
-    document.getElementById('num-followers').innerText = e.followers;
-  })
-  .then(function(e) {
-    console.log(e);
-    document.getElementById('loading').innerText = 'Loading...';
-  }); 
-  // display avatar, username, real name, location, bio, and num followers 
-  // get username and avatar of followers 
-  // add a loading indicator 
-  // add an input box that takes in other names to display 
+var response = null;
+var followers = null;
+
+document.getElementsByTagName('button')[0].addEventListener('click', function(r) {
+  getUser(document.getElementsByTagName('input')[0].value);
+});
+
+
+function getUser(name) {
+  fetch('https://api.github.com/users/' + name)
+    .then(function(r) {
+      return r.json();
+    })
+    .then(function(j) {
+      response = j;
+      assignValues();
+      getFollowers(j.followers_url);
+    })
+}
+
+// a bunch of selectors, my more intutitve way to do it 
+function assignValues() {
+  document.getElementById('loader').style = 'display: none';
+
+  document.getElementById('avatar').src = response.avatar_url;
+  document.getElementById('name').innerText = response.name;
+  document.getElementById('username').innerText = response.login;
+  document.getElementById('location').innerText = response.location;
+  document.getElementById('bio').innerText = response.bio;
+  document.getElementById('count').innerText = 'Followers: ' + response.followers;
+}
+
+function getFollowers(url) {
+  fetch(url)
+    .then(function(r) {
+      return r.json();
+    })
+    .then(function(f) {
+      followers = f;
+      listFollowers();
+    });
+}
+
+// standard foreach loop --> creates a list element 
+function listFollowers() {
+  followers.forEach(function(f) {
+    var li = document.createElement('li');
+    // url of the follower 
+    li.innerHTML = '<a href="' + f.html_url + '">'
+    // if you hover on the follower you get their username 
+      + '<img src="' + f.avatar_url + '" alt="' + f.login + '"/>'
+      + '</a>';
+    document.getElementById('list').appendChild(li);
+  });
+}
